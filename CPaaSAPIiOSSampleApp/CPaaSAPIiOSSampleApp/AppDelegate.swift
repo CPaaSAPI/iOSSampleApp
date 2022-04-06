a3a928ab93c8ed313941a3ae681a8880c7ffe3ae
Oleg Arzutov <oleg.arzutov@mavenir.com>
2022-04-06 15:58:02 +0300

    CSDK-294_Sample_App Changes

--- CPaaSAPIiOSSampleApp/CPaaSAPIiOSSampleApp/AppDelegate.swift
+++ CPaaSAPIiOSSampleApp/CPaaSAPIiOSSampleApp/AppDelegate.swift
@@ -13,9 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UIHostingController(rootView:MainView())
-        window?.makeKeyAndVisible()
         return true
     }
 
