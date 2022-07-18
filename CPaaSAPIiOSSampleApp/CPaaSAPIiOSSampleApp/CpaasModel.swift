1f0a40694e8f80930c75bd2ba116854a28f353e1
Nachum Spigelman <nachum.spigelman@mavenir.com>
2022-07-18 14:12:32 +0300

    CSDK-497
    Support new SDK voice

--- CPaaSAPIiOSSampleApp/CPaaSAPIiOSSampleApp/CpaasModel.swift
+++ CPaaSAPIiOSSampleApp/CPaaSAPIiOSSampleApp/CpaasModel.swift
@@ -7,45 +7,100 @@
 
 import Foundation
 
-import CpaasAPI
+import CpaaSAPI
 import SwiftUI
 
 
 class CpaasModel: ObservableObject {
     
-let custom_domain = "webrtc-dev.restcomm.com"
-let app_token     = "token"
-let account_sid   = "sid"
-let app_sid       = "ClickToCallDevApp"
-let pns_token     = "PNSTOKEN"
-let base_url      = Const.shared.WS_URL_AWS
-var call: ICall?
-@Published var showCallView: Bool = false
-@Published var isCallMuted: Bool = false
-    func register(userID: String){
-        let settings = CPaaSAPISettings(customDomain: custom_domain, accountSid: account_sid, accountToken: app_token , appSid: app_sid, clientId: userID, PNSToken:pns_token, BaseURL: base_url)
-            CPaaSAPI.shared.register(settings: settings, cpaasAPICb: self)
-        }
-   
+    let userID: String = "YOUR_USER_ID"
+    let custom_domain  = "webrtc-dev.restcomm.com"
+    let account_sid    = "AC3c5b4177e5fdd813720bc0d6dd7f057e"
+    let auth_token     = "c8b0fca2c59d9198b641ce60fe9b501b"
+    let app_sid        = "ClickToCallDevApp"
+    let pns_token      = "PNSTOKEN"
+    let base_url       = Const.shared.HTTP_URL_AWS
     
-    func start(destinationID: String) {
-        call = CPaaSAPI.shared.startCall(destinationId: destinationID, callOptions:  CallOptions(audio: true))
-        guard let call = call  else{return}
-        call.eventListener = self
-        showCallView = true
+    var call: CPaaSCall?
+    
+    @Published var showCallView: Bool = false
+    @Published var isCallMuted: Bool = false
+    @Published var isLoadingActivated: Bool = false
+    @Published var strStatusCall: String = "Initiating Call ..."
+    var statusCall: CallStatus = .callEnd {
+        didSet {
+            switch statusCall
+            {
+            case .calling:
+                strStatusCall = "Calling ..."
+            case .initCall:
+                strStatusCall = "Initiating Call ..."
+            case .connected:
+                strStatusCall = "Connected"
+            case .callEnd:
+                strStatusCall = "End Call"
+            case .callReconnecting:
+                strStatusCall = "Reconnecting"
+                
+            }
+        }
+    }
+    
+    func register() {
+        let settings = CPaaSAPISettings(customDomain: custom_domain, accountSid: account_sid, authToken: auth_token, appSid: app_sid, clientId: userID, PNSToken: pns_token, baseURL: base_url)
+        CPaaSAPI.shared.register(settings: settings, cpaasAPICb: self)
+    }
+    
+    
+    func start() {
+        statusCall = .initCall
+        CPaaSAPI.shared.voice.create { [weak self] createResult in
+            guard let self = self else { return }
+            
+            createResult.fold { callId in
+                self.connect(callId: callId! as String)
+            } onFailure: { error in
+                print("Failed to create call - not registered")
+            }
+        }
+    }
+    
+    private func connect(callId: String) {
+        
+        let api = CPaaSAPI.shared.voice
+
+        api.connect(callId: callId, callOptions: CallOptions(audio: true)) { [weak self] connectResult in
+            guard let self = self else { return }
+            connectResult.fold { newCall in
+                self.call = newCall
+                newCall?.eventListener = self
+                self.showCallView = true
+            } onFailure: { callStartError in
+                print("Failed connecting to meeting: \(callStartError.debugDescription)")
+                switch callStartError {
+                case is CallStartError.CallAlreadyExistError:
+                    break
+                case is CallStartError.MicrophonePermissionError:
+                    break
+                default:
+                    break
+                }
+            }
+
+        }
     }
     
     func endCall(){
         guard let call = call  else{return}
         call.endCall()
-     
+        
     }
     
     func mute() {
         guard let call = call  else{return}
         if !isCallMuted {
             call.mute { succeed in
-            print("called mute, succeed: \(succeed)")
+                print("called mute, succeed: \(succeed)")
                 if succeed == true {
                     self.isCallMuted = true
                 }
@@ -56,53 +111,63 @@ var call: ICall?
                 if succeed == true {
                     self.isCallMuted = false
                 }
-                
             }
         }
     }
-
-}
-
-
-extension CpaasModel: CPaaSAPICb {
-
-    func onIncomingCall(call: ICall) {
-        print("When there is an incoming call, you can use the  ``call`` object to accept it and call ``call.join()`` to join it.")
-        self.call = call
-        self.call!.eventListener = self
-        self.call!.joinCall()
-        showCallView = true
-    }
     
-    func onRegistrationComplete(success: Bool) {
-        print("Registration is successful, ready to start call")
-    }
-}
-
-extension CpaasModel: ICallEvents {
-    func onCallEnd(reason: Reason?) {
+    func endCallHandler() {
+        statusCall = .callEnd
         showCallView = false
         call = nil
         print("Call end")
     }
+}
+
+
+extension CpaasModel: CPaaSAPICb {
+    func onIncomingCall(callId: String, callerId: String, serviceType: ServiceType) {
+        statusCall = .initCall
+        //When receiving an incoming call you can accept it by calling CPaaSAPI.shared.connect or reject it by calling CPaaSAPI.shared.reject
+        connect(callId: callId)
+    }
+    
+    func onRegistrationComplete(success: Bool) {
+        print("Registration finished: \(success)")
+    }
+}
+
+extension CpaasModel: CPaaSCallEvents {
+    func onCallEnd(reason: CPaaSReason) {
+        endCallHandler()
+    }
     
     func onConnected() {
         print("Call connected")
+        isLoadingActivated = false
+        statusCall = .connected
     }
     
-    func onConnectedFailure(reason: Reason) {
-        showCallView = false
-        call = nil
+    func onConnectedFailure(reason: CPaaSReason) {
+        endCallHandler()
         print("Connection failed the reson is :\(reason)")
     }
     
-    func onReconnecting(reason: Reason) {
+    func onReconnecting(reason: CPaaSReason) {
         print("Call on reconnecting proces the reason of reconnecting is :\(reason)")
+        statusCall = .callReconnecting
     }
     
     func onRinging() {
         print("Callee got your call, waiting for his action (accept/reject)")
+        statusCall = .calling
     }
 }
 
 
+enum CallStatus: String {
+    case initCall
+    case calling
+    case connected
+    case callEnd
+    case callReconnecting
+}
--- CPaaSAPIiOSSampleApp/CPaaSAPIiOSSampleApp/MainView.swift
+++ CPaaSAPIiOSSampleApp/CPaaSAPIiOSSampleApp/MainView.swift
@@ -6,51 +6,42 @@
 //
 
 import SwiftUI
-import CpaasAPI
+import CpaaSAPI
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
 let bgButtonColor = Color(red:50.0/255.0, green: 64.0/255.0, blue: 73.0/255.0, opacity: 1.0)
 struct MainView: View {
-    var userID: String = "YOUR_USER_ID"
-    var destinationID: String = "DESTINATION_ID"
 
     @EnvironmentObject var model :CpaasModel
     var body: some View {
         NavigationView {
-            VStack{
+            VStack(spacing: 50) {
                Image("Mavenir-Logo")
-               .resizable()
-               .aspectRatio(12/4, contentMode: .fit)
-               .padding([.leading,.trailing,.bottom],50)
-                
-                Button (action: {
-                    model.register(userID: userID)
-                } ){
-                    Text("Registration")
+
+                VStack(spacing: 10) {
+                    Button (action: {
+                        model.register()
+                    } ){
+                        Text("Registration")
+                            .font(.headline)
+                            .frame(width: 250.0, height: 60.0).foregroundColor(.white)
+                            .background(bgButtonColor)
+                            .cornerRadius(8.0)
+                    }
+                    Button (action: {
+                        model.start()
+                    } ){
+                        Text("Start Call")
+                            .font(.headline)
+                            .frame(width: 250.0, height: 60.0).foregroundColor(.white)
+                            .background(bgButtonColor)
+                            .cornerRadius(8.0)
+                    }
                 }
-                .font(.headline)
-                .frame(width: 250.0, height: 60.0).foregroundColor(.white)
-                .background(bgButtonColor)
-                .cornerRadius(8.0)
-                .padding(.bottom,10)
-                
-                Button (action: {
-                    model.start(destinationID: destinationID)
-                } ){
-                    Text("Start Call")
-                }
-                .font(.headline)
-                .frame(width: 250.0, height: 60.0).foregroundColor(.white)
-                .background(bgButtonColor)
-                .cornerRadius(8.0)
                 
                 Spacer()
                 NavigationLink(destination:  CallView().environmentObject(model), isActive: $model.showCallView) {}.isDetailLink(false)
-                
             }
-            .padding()
         }
-   
     }
-
 }
 
